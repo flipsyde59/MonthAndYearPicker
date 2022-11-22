50ce25d3581b5a352ffb079b6ece6ac0291ef171
firdausmaulan <firdausmaulan@gmail.com>
2022-11-22 20:14:35 +0700

    update dependencies

--- monthandyearpicker/src/main/java/com/whiteelephant/monthpicker/YearPickerView.java
+++ monthandyearpicker/src/main/java/com/whiteelephant/monthpicker/YearPickerView.java
@@ -2,9 +2,8 @@ package com.whiteelephant.monthpicker;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.support.annotation.DrawableRes;
+import androidx.annotation.DrawableRes;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
